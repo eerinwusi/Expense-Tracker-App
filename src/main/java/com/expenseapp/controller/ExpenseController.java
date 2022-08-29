e37e765d51f4a16616c2227240b5d043cdd16885
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-08-29 15:50:30 +0100

    Create a REST endpoint for Expenses

---
+++ src/main/java/com/expenseapp/controller/ExpenseController.java
@@ -0,0 +1,12 @@
+package com.expenseapp.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ExpenseController {
+    @GetMapping("/expenses")
+    public String getAllExpenses() {
+        return "List all expenses";
+    }
+}
