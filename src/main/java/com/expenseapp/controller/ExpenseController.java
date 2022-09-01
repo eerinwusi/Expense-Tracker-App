2757164b71cc7a38b1dc5bea617a4ddec100e315
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-01 15:02:18 +0100

    Using Request Param in URLs

--- src/main/java/com/expenseapp/controller/ExpenseController.java
+++ src/main/java/com/expenseapp/controller/ExpenseController.java
@@ -3,9 +3,7 @@ package com.expenseapp.controller;
 import com.expenseapp.model.Expense;
 import com.expenseapp.service.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -20,8 +18,15 @@ public class ExpenseController {
         return expenseService.getAllExpenses();
     }
 
+//    using path variable
     @GetMapping("/expenses/{id}")
     public String getExpenseById(@PathVariable("id") Long id) {
         return "The expense is " + id;
+    }
+
+//    using request param.
+    @DeleteMapping("/expenses")
+    public String deleteExpenseById(@RequestParam("id") Long id) {
+        return "Delete object with id - " + id;
     }
 }
