a59859e870d8e0853cf24e6b5b7c3dd4557801a6
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-06 01:47:49 +0100

    Customize error response for Validations

--- src/main/java/com/expenseapp/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/expenseapp/exceptions/GlobalExceptionHandler.java
@@ -1,17 +1,24 @@
 package com.expenseapp.exceptions;
 
 import com.expenseapp.model.ErrorObject;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
-public class GlobalExceptionHandler {
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
 
 //    handles exceptions such as inputing an id that does not exist in the db.
     @ExceptionHandler(ExpenseNotFoundException.class)
@@ -55,5 +62,22 @@ public class GlobalExceptionHandler {
         errorObject.setTimeStamp(new Date());
 
         return new ResponseEntity<ErrorObject>(errorObject, HttpStatus.INTERNAL_SERVER_ERROR);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders httpHeaders,
+    HttpStatus httpStatus, WebRequest request) {
+        Map<String, Object> body = new HashMap<String, Object>();
+
+        body.put("timeStamp", new Date());
+
+        body.put("statusCode", HttpStatus.BAD_REQUEST.value());
+
+        List<String> errors = ex.getBindingResult()
+                        .getFieldErrors().stream().map(x -> x.getDefaultMessage()).collect(Collectors.toList());
+
+        body.put("message", errors);
+
+        return new ResponseEntity<Object>(body, HttpStatus.BAD_REQUEST);
     }
 }
