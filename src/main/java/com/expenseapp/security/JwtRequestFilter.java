b3bd859cc2b0748ecc7d1242eb5f13d4d48fcb18
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-07 18:27:14 +0100

    Create a custom filter

---
+++ src/main/java/com/expenseapp/security/JwtRequestFilter.java
@@ -0,0 +1,39 @@
+package com.expenseapp.security;
+
+import com.expenseapp.util.JwtTokenUtil;
+import io.jsonwebtoken.ExpiredJwtException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class JwtRequestFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        final String requestTokenHeader = request.getHeader("Authorization");
+
+        String jwtToken = null;
+        String username = null;
+
+        if (requestTokenHeader != null && requestTokenHeader.startsWith("Bearer ")) {
+            jwtToken = requestTokenHeader.substring(7);
+
+            try {
+                username = jwtTokenUtil.getUsernameFromToken(jwtToken);
+            } catch (IllegalArgumentException exception) {
+                throw new RuntimeException("Unable to get JWT token");
+            } catch (ExpiredJwtException exception) {
+                throw new RuntimeException("JWT token has expired");
+            }
+        }
+    }
+
+}
