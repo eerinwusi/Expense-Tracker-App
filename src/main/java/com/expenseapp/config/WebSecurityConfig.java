da6c6b69ee93945021c3710a7bc75b54707e053e
Emmanuel Erinwusi <Emmanuel.Erinwusi@etranzact.com>
2022-09-07 14:56:55 +0100

    Code for customizing multiple users

--- src/main/java/com/expenseapp/config/WebSecurityConfig.java
+++ src/main/java/com/expenseapp/config/WebSecurityConfig.java
@@ -1,10 +1,19 @@
 package com.expenseapp.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
+import org.springframework.security.config.ldap.LdapBindAuthenticationManagerFactory;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -21,5 +30,31 @@ public class WebSecurityConfig {
                 .and()
                 .httpBasic();
         return http.build();
+    }
+
+//    @Bean
+//    public void configure(AuthenticationManagerBuilder authenticationManager) {
+//        authenticationManager.inMemoryAuthentication()
+//                .withUser("Software Engineer").password("12345").authorities("admin")
+//                .and()
+//                .withUser("programmer1").password("12345").authorities("user")
+//                .passwordEncoder(NoOpPasswordEncoder.getInstance());
+//    }
+
+    @Bean
+    public InMemoryUserDetailsManager userDetailsService() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("Software Engineer")
+                .password("12345")
+                .roles("admin")
+                .build();
+
+        UserDetails user1 = User.withDefaultPasswordEncoder()
+                .username("programmer1")
+                .password("12345")
+                .roles("user")
+                .build();
+
+        return new InMemoryUserDetailsManager(user, user1);
     }
 }
